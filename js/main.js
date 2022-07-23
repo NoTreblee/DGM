a6aa659aa6f342c103d82fac77002d6d171c4448
NoTreblee <42041720+NoTreblee@users.noreply.github.com>
2022-07-23 12:47:27 +0200

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ const navLinks = document.getElementsByClassName('nav__link');
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer__year');
+const body = document.getElementById('body');
+
+mybutton = document.getElementById('myBtn');
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 const handleCurrentYear = () => {
   const year = new Date().getFullYear();
@@ -11,11 +18,13 @@ handleCurrentYear();
 const handleNav = () => {
   navBtn.classList.toggle('is-active');
   navMobile.classList.toggle('nav-mobile--active');
+  body.classList.toggle('fixed');
 };
 for (const navLink of navLinks) {
   navLink.addEventListener('click', function closeNav() {
     navMobile.classList.remove('nav-mobile--active');
     navBtn.classList.toggle('is-active');
+    body.classList.toggle('fixed');
   });
 }
 navBtn.addEventListener('click', handleNav);
